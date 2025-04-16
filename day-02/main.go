9b59e5217315089d4e460fa900780db2a12cdef9
eduardo-eiji-goto <eduardo.e.goto@gmail.com>
2025-04-16 19:48:38 -0300

    day 02.2

--- day-02/main.go
+++ day-02/main.go
@@ -9,6 +9,9 @@ import (
 	"strings"
 )
 
+//* Day 02: Red-Nosed Reports
+//* https://adventofcode.com/2024/day/2
+
 func main() {
 	reports, err := readInput("input.txt")
 	if err != nil {
@@ -16,6 +19,7 @@ func main() {
 	}
 
 	fmt.Println("Safe Reports:", getSafeReportCount(reports))
+	fmt.Println("Safe Reports (Problem Dampener):", getSafeReportCountDampener(reports))
 }
 
 func getSafeReportCount(reports [][]int) int {
@@ -53,6 +57,77 @@ func getSafeReportCount(reports [][]int) int {
 	}
 
 	return safeReportCount
+}
+
+func getSafeReportCountDampener(reports [][]int) int {
+	var safeReportCount int
+
+	for i := range reports {
+		reportCurrent := reports[i]
+		isReportSafe, violations := validateReport(reportCurrent)
+
+		if isReportSafe != true {
+			violationPossibilities := []int{0, len(reportCurrent) - 1}
+			violationPossibilities = append(violationPossibilities, violations...)
+
+			for _, violationId := range violationPossibilities {
+				var reportDampener []int
+				reportDampener = append(reportDampener, reportCurrent[:violationId]...)
+				reportDampener = append(reportDampener, reportCurrent[violationId+1:]...)
+
+				isReportSafeDampener, _ := validateReport(reportDampener)
+
+				if isReportSafeDampener == true {
+					isReportSafe = true
+				}
+			}
+		}
+
+		if isReportSafe == true {
+			safeReportCount += 1
+		}
+	}
+
+	return safeReportCount
+}
+
+func isItemValid(report []int, index int) bool {
+	differencePrevious := report[index] - report[index-1]
+	differencePreviousAbs := math.Abs(float64(differencePrevious))
+	if differencePreviousAbs < 1 || differencePreviousAbs > 3 {
+		return false
+	}
+
+	differenceNext := report[index+1] - report[index]
+	differenceNextAbs := math.Abs(float64(differenceNext))
+	if differenceNextAbs < 1 || differenceNextAbs > 3 {
+		return false
+	}
+
+	if math.Signbit(float64(differencePrevious)) != math.Signbit(float64(differenceNext)) {
+		return false
+	}
+
+	return true
+}
+
+func validateReport(report []int) (bool, []int) {
+	isValid := true
+	var violations []int
+
+	for i := range report {
+		if i > 0 && i < len(report)-1 {
+			if isItemValid(report, i) == false {
+				violations = append(violations, i)
+			}
+		}
+	}
+
+	if len(violations) > 0 {
+		isValid = false
+	}
+
+	return isValid, violations
 }
 
 func readInput(name string) ([][]int, error) {
